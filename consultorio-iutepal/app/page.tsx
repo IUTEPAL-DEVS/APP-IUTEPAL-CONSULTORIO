261d7d526b8f375e52c1d3f94968cac9230e8679
Ricardo Chavez <chavezmataricardo@gmail.com>
2024-10-31 16:49:18 -0400

    feat: Integrate Supabase client and environment configuration

--- consultorio-iutepal/app/page.tsx
+++ consultorio-iutepal/app/page.tsx
@@ -3,3 +3,29 @@ import Login from '@/components/login-form';
 export default function Home() {
   return <Login />;
 }
+
+
+// import { createClient } from '@/utils/supabase/server'
+// import { cookies } from 'next/headers'
+
+// export default async function Page() {
+//   const cookieStore = await cookies()
+//   const supabase = createClient(cookieStore)
+
+//   const { data: todos } = await supabase.from('tipo_paciente').select()
+
+//   return (
+//     <>
+//       <div>
+//         holaaaa
+//       </div>
+//       <ul>
+//         {todos?.map((todo) => (
+//           <li>{todo.name}</li>
+
+//         ))}
+//       </ul>
+//     </>
+
+//   )
+// }
