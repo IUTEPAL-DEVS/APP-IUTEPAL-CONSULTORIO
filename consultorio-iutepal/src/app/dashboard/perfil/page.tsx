d758e068620817ff88c66b3f1ba1616adb3f28f3
Ricardo Chavez <chavezmataricardo@gmail.com>
2024-11-01 09:48:31 -0400

    feat: Fetch and display user profile information on the perfil page

--- consultorio-iutepal/src/app/dashboard/perfil/page.tsx
+++ consultorio-iutepal/src/app/dashboard/perfil/page.tsx
@@ -1,8 +1,28 @@
-export default function Page() {
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+
+export default async function Page() {
+    const cookieStore = cookies();
+    const supabase = createRouteHandlerClient({
+        cookies: () => cookieStore
+    });
+
+    const { data: { user } } = await supabase.auth.getUser();
+    console.log(user);
+
     return (
         <div>
             <h1>Perfil</h1>
-            <p>Esta es la pagina de perfil</p>
+            {user ? (
+                <div>
+                    <p>ID: {user.id}</p>
+                    <p>Email: {user.email}</p>
+                    <p>Nombre: {user.phone}</p>
+                    {/* Add other user properties as needed */}
+                </div>
+            ) : (
+                <p>No user data available</p>
+            )}
         </div>
-    )
+    );
 }
